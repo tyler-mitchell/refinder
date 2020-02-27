44f1c8e1a9daa9129bb25c4db2716b762fbe6f99
Tyler <tdmam404@outlook.com>
2020-02-27 01:53:54 -0600

    create prototype of dashboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
 import React from "react";
 import { Counter } from "./features/counter/Counter";
+import app from "firebase/app";
+import firebase from "firebase";
+import Products from "./features/ecommerce/Products";
+import { Toolbar, Typography } from "@material-ui/core";
+import {
+  Root,
+  Header,
+  Sidebar,
+  Content,
+  Footer,
+  CollapseBtn,
+  CollapseIcon,
+  SidebarTrigger,
+  cozyLayoutPreset,
+  SidebarTriggerIcon
+} from "@mui-treasury/layout";
 import "./App.css";
-
+import NavContent from "./NavContent";
+var firebaseConfig = {
+  apiKey: "AIzaSyBdhd3g3f8DkpMB8DmmLrlXjpVLyOUkngw",
+  authDomain: "refinder-exchange.firebaseapp.com",
+  databaseURL: "https://refinder-exchange.firebaseio.com/",
+  projectId: "refinder-exchange",
+  storageBucket: "refinder-exchange.appspot.com",
+  messagingSenderId: "795224011963",
+  appId: "1:795224011963:web:06a200ceb6087f4d32a59e"
+};
+// Initialize Firebase
+if (!app.apps.length) {
+  app.initializeApp(firebaseConfig);
+}
 function App() {
   return (
     <div className="App">
-      <Counter />
+      <Root config={cozyLayoutPreset}>
+        <Header>
+          <Toolbar>
+            <SidebarTrigger>
+              <SidebarTriggerIcon />
+            </SidebarTrigger>
+            <Typography variant="h6">Refinder</Typography>
+          </Toolbar>
+        </Header>
+        <Content>
+          <Products />
+        </Content>
+        <Sidebar>
+          <NavContent />
+        </Sidebar>
+        <Footer>Footer!</Footer>
+      </Root>
     </div>
   );
 }
---
+++ src/NavContent.js
@@ -0,0 +1,113 @@
+import React from "react";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import Icon from "@material-ui/core/Icon";
+import Divider from "@material-ui/core/Divider";
+import Folder from "@material-ui/icons/Folder";
+import People from "@material-ui/icons/People";
+import Star from "@material-ui/icons/Star";
+import Schedule from "@material-ui/icons/Schedule";
+import OfflinePin from "@material-ui/icons/OfflinePin";
+import Publish from "@material-ui/icons/Publish";
+import Backup from "@material-ui/icons/Backup";
+import Delete from "@material-ui/icons/Delete";
+import Settings from "@material-ui/icons/Settings";
+import ListSubheader from "@material-ui/core/ListSubheader";
+
+const list = [
+  {
+    primaryText: "Wood",
+    icon: <Folder />
+  },
+  {
+    primaryText: "Glass",
+    icon: <People />
+  },
+  {
+    primaryText: "Brick",
+    icon: <Star />
+  },
+  {
+    primaryText: "Concrete",
+    icon: <Star />
+  },
+  {
+    primaryText: "Doors",
+    icon: <Star />
+  },
+  {
+    primaryText: "Windows",
+    icon: <Star />
+  },
+  {
+    primaryText: "Fabrics",
+    icon: <Schedule />
+  },
+  {
+    primaryText: "Carpet",
+    icon: <Schedule />
+  },
+  {
+    primaryText: "Paper and Cardboard",
+    icon: <OfflinePin />
+  },
+  {
+    primaryText: "Metals",
+    icon: <Publish />
+  },
+  {
+    primaryText: "Organics",
+    icon: <Backup />
+  },
+  {
+    primaryText: "Rubber",
+    icon: <Backup />
+  },
+  {
+    primaryText: "Electronics",
+    icon: <Backup />
+  },
+
+  {
+    primaryText: "Furniture",
+    icon: <Backup />
+  },
+  {
+    primaryText: "Construction and Demolition",
+    icon: <Delete />
+  }
+];
+const NavContent = ({ onClickItem }) => (
+  <List>
+    <ListSubheader>All Categories</ListSubheader>
+    {list.map(({ primaryText, icon }, i) => (
+      <ListItem
+        key={primaryText}
+        selected={i === 0}
+        button
+        onClick={onClickItem}
+      >
+        {/* <ListItemIcon>
+          <Icon>{icon}</Icon>
+        </ListItemIcon> */}
+        <ListItemText
+          primary={primaryText}
+          primaryTypographyProps={{ noWrap: true }}
+        />
+      </ListItem>
+    ))}
+    <Divider style={{ margin: "12px 0" }} />
+    <ListItem button onClick={onClickItem}>
+      <ListItemIcon>
+        <Settings />
+      </ListItemIcon>
+      <ListItemText
+        primary={"Settings & account"}
+        primaryTypographyProps={{ noWrap: true }}
+      />
+    </ListItem>
+  </List>
+);
+export default NavContent;
