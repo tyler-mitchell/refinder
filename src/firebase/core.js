ae8983ecf37e64123055cd46e376016c91eb23a5
Tyler Mac <tdmam404@outlook.com>
2020-03-15 19:21:32 -0500

    add error check to firebase

--- src/firebase/core.js
+++ src/firebase/core.js
@@ -14,7 +14,10 @@ const firebaseConfig = {
 };
 
 
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
 
 const auth = firebase.auth();
 const database = firebase.firestore();
