f7904e013e777f0a9c8fca6cc99a3ae58ef086c1
Tyler <tdmam404@outlook.com>
2020-03-22 02:41:43 -0500

    add labels to sidebar

--- src/views/MarketplaceView/SidebarContent.js
+++ src/views/MarketplaceView/SidebarContent.js
@@ -2,35 +2,40 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 import DashboardIcon from "@material-ui/icons/DashboardRounded";
-import MaterialIcon from "@material-ui/icons/Grain";
+import MaterialIcon from "@material-ui/icons/Terrain";
 import SettingsIcon from "@material-ui/icons/SettingsOutlined";
 import ServicesIcon from "@material-ui/icons/Build";
-import SellIcon from "@material-ui/icons/MonetizationOn";
+import SellIcon from "@material-ui/icons/LocalAtm";
 import {
-  ListItem,
   List,
+  ListItem,
+  ListItemText,
   Link,
   ListItemAvatar,
   Tooltip
 } from "@material-ui/core";
 
 const navigationLinks = [
-  { primaryText: "Dashboard", icon: <DashboardIcon />, path: "" },
-  { primaryText: "Dashboard", icon: <SellIcon />, path: "sell" },
-  { primaryText: "Materials", icon: <MaterialIcon />, path: "materials" }
+  { label: "Dashboard", icon: <DashboardIcon />, path: "" },
+  { label: "Materials", icon: <MaterialIcon />, path: "materials" },
+  { label: "Sell", icon: <SellIcon />, path: "sell" }
 ];
 const SidebarContent = () => {
   const navigate = useNavigate();
   return (
     <List>
-      {navigationLinks.map(({ icon, path }) => (
+      {navigationLinks.map(({ icon, path, label }) => (
         <ListItem
           button
+          key={label}
           onClick={() => {
             navigate(path);
           }}
         >
           <ListItemAvatar>{icon}</ListItemAvatar>
+          <ListItemText>
+            <span style={{ fontWeight: 600 }}>{label}</span>
+          </ListItemText>
         </ListItem>
       ))}
     </List>
